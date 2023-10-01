f0d8f10544ff4b51b8d7ec06c9392bf54185527a
FawnPython <126659381+FawnPython@users.noreply.github.com>
2023-10-01 16:19:39 +0100

    Update Blade Ball Mobile vorleta.lua

--- Blade Ball Mobile vorleta.lua
+++ Blade Ball Mobile vorleta.lua
@@ -6,10 +6,10 @@ local b = w:CreateFolder("Spam")
 
 e:Toggle("Auto Click", function(bool)
     Toggle = bool;
-    end)
+end)
 spawn(function()
-        while wait() do
-            
+        while wait(0.3) do
+         if Toggle then   
             local args = {
             [1] = 0.5,
             [2] = CFrame.new(-157.66162109375, 17.132680892944336, -114.03600311279297, 0.8926612734794617, 0.2507196366786957, -0.37456053495407104, 1.3797912146173985e-08, 0.831011950969696, 0.5562545657157898, 0.45072823762893677, -0.4965468943119049, 0.7418121099472046),
